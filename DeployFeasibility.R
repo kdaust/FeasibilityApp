57102a92725f1151c6261a7d7f4cdb78ac0e2be7
Kiri Daust <kiridaust@gmail.com>
2021-05-21 18:07:49 -0700

    Renamed

--- DeployFeasibility.R
+++ DeployFeasibility.R
@@ -4,8 +4,10 @@ Sys.setenv(DO_PAT="eae4166ed2fac0e3c41660fe26a009bb0176ab8bceeaf753faf5189f58a06
 temp <- analogsea::droplets()
 server <- temp$`shiny-server`
 analogsea::droplet_ssh(server, "mkdir /srv/shiny-server/feasibility")
-analogsea::droplet_upload(server, "./app/index.Rmd", "/srv/shiny-server/cciss/index.Rmd")
-analogsea::droplet_upload(server, "./app/www", "/srv/shiny-server/cciss")
-analogsea::droplet_upload(server, "./app/server", "/srv/shiny-server/cciss")
+analogsea::droplet_upload(server, "./app.R", "/srv/shiny-server/feasibility/app.R")
+analogsea::droplet_upload(server, "./FeasAppSource.R", "/srv/shiny-server/feasibility/FeasAppSource.R")
+analogsea::droplet_upload(server, "./www", "/srv/shiny-server/feasibility")
+analogsea::droplet_upload(server, "./htmlwidgets", "/srv/shiny-server/feasibility")
+analogsea::droplet_upload(server, "./subzones_colours_ref.rda", "/srv/shiny-server/feasibility/subzones_colours_ref.rda")
 analogsea::droplet_ssh(server, "chown -R shiny:shiny /srv/shiny-server")
 analogsea::droplet_ssh(server, "systemctl restart shiny-server")
